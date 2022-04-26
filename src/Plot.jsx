3fac82afa3faa4956f8d913b40714cb4577b9dd2
kersh1337228 <antonycherevko@gmail.com>
2022-04-27 02:50:18 +0300

    Objects drawn paths are now saved. Line smoothing improved.

--- src/Plot.jsx
+++ src/Plot.jsx
@@ -24,6 +24,7 @@ class Axis {
             },
             color: '#000000'
         }
+        this.path = null
     }
     get_labels_font(density) { // Canvas formatted label style
         return `${this.labels.font.style} ${this.labels.font.size * density}px ${this.labels.font.fontFamily}`
@@ -35,8 +36,8 @@ class Axis {
 class Figure {
     constructor() {
         this.window_size = {
-            width: 600,
-            height: 200,
+            width: 850,
+            height: 480,
         }
         this.plot_density = {
             width: 1,
@@ -49,9 +50,10 @@ class Figure {
             bottom: 0.1
         }
         this.style = {
-            color: '#002aff',
+            color: '#000000',
             width: 2
         }
+        this.path = null
     }
     // Canvas actual width
     get_width() {
@@ -90,10 +92,18 @@ export default class Plot extends React.Component {
                 y: new Axis(),
             },
             grid: {
-                horizontalAmount: 10,
-                verticalAmount: 10,
-                color: '#d9d9d9',
-                width: 1,
+                vertical: {
+                    amount: 10,
+                    color: '#d9d9d9',
+                    width: 1,
+                    path: null,
+                },
+                horizontal: {
+                    amount: 10,
+                    color: '#d9d9d9',
+                    width: 1,
+                    path: null,
+                },
             }
         }
         this.type = props.type
@@ -123,7 +133,6 @@ export default class Plot extends React.Component {
         // Linear transformation choose depending on plot type
         switch (this.type) {
             case 'date_value':
-                this.show_grid() // Translucent grid
                 for (let i = 0; i < original_data.x.length; ++i) {
                     x.push(
                         this.state.axes.x.center +
@@ -137,8 +146,6 @@ export default class Plot extends React.Component {
                 }
                 break
             default:
-                this.show_grid() // Translucent grid
-                this.show_axes() // Axes and axes labels
                 for (const [xi, yi] of zip(original_data.x, original_data.y)) {
                     x.push(this.state.axes.x.center + xi / width_scale)
                     y.push(this.state.axes.y.center - yi / height_scale)
@@ -193,32 +200,42 @@ export default class Plot extends React.Component {
     }
     // Date-value type plot (<date:String>, <value:Number>)
     plot_date_value() {
-        const data = this.state.data
-        const context = this.canvas.current.getContext('2d')
-        // Drawing plot
-        context.beginPath()
-        context.lineWidth = this.state.figure.get_line_width()
-        context.strokeStyle = this.state.figure.style.color
-        context.moveTo(data.x[0], data.y[0])
-        for (const [x, y] of zip(data.x, data.y)) {
-            context.lineTo(x, y)
-        }
-        context.stroke()
-        context.closePath()
+        this.show_grid(() => { // Translucent grid
+            const context = this.canvas.current.getContext('2d')
+            // Drawing plot
+            let figure_path = new Path2D()
+            context.lineWidth = this.state.figure.get_line_width()
+            context.strokeStyle = this.state.figure.style.color
+            context.lineJoin = 'round'
+            figure_path.moveTo(this.state.data.x[0], this.state.data.y[0])
+            for (const [x, y] of zip(this.state.data.x, this.state.data.y)) {
+                figure_path.lineTo(x, y)
+            }
+            context.stroke(figure_path)
+            let figure = this.state.figure
+            figure.path = figure_path
+            this.setState({figure: figure})
+        })
     }
     // Math type plot (<x:Number>, <y:Number>)
     plot_math() {
-        const context = this.canvas.current.getContext('2d')
-        // Drawing plot
-        context.beginPath()
-        context.lineWidth = this.state.figure.get_line_width()
-        context.strokeStyle = this.state.figure.style.color
-        context.moveTo(this.state.data.x[0], this.state.data.y[0])
-        for (const [x, y] of zip(this.state.data.x, this.state.data.y)) {
-            context.lineTo(x, y)
-        }
-        context.stroke()
-        context.closePath()
+        this.show_grid(() => { // Translucent grid
+            this.show_axes(() => { // Axes and axes labels
+                const context = this.canvas.current.getContext('2d')
+                // Drawing plot
+                let figure_path = new Path2D()
+                context.lineWidth = this.state.figure.get_line_width()
+                context.strokeStyle = this.state.figure.style.color
+                figure_path.moveTo(this.state.data.x[0], this.state.data.y[0])
+                for (const [x, y] of zip(this.state.data.x, this.state.data.y)) {
+                    figure_path.lineTo(x, y)
+                }
+                context.stroke(figure_path)
+                let figure = this.state.figure
+                figure.path = figure_path
+                this.setState({figure: figure})
+            })
+        })
     }
     // Financial type plot (
     //      <date:String>,
@@ -228,22 +245,22 @@ export default class Plot extends React.Component {
 
     }
     // Show coordinate axes and data labels
-    show_axes() {
+    show_axes(callback) {
         // Data binding
         const x_axis = this.state.axes.x
         const y_axis = this.state.axes.y
         // Axes visibility check
         if (x_axis.visible || y_axis.visible) {
             const context = this.canvas.current.getContext('2d')
-            context.beginPath()
             // Drawing horizontal (x)
             if (x_axis.visible) {
+                let x_axis_path = new Path2D()
                 // Style
                 context.lineWidth = x_axis.width * this.state.figure.plot_density.height
                 context.strokeStyle = x_axis.color
                 // Axis line
-                context.moveTo(0, y_axis.center)
-                context.lineTo(this.state.figure.get_width(), y_axis.center)
+                x_axis_path.moveTo(0, y_axis.center)
+                x_axis_path.lineTo(this.state.figure.get_width(), y_axis.center)
                 // Horizontal labels (width levels)
                 if (x_axis.labels.visible) {
                     context.font = x_axis.get_labels_font(this.state.figure.plot_density.width)
@@ -261,19 +278,22 @@ export default class Plot extends React.Component {
                             )
                         }
                         // Vertical hatch
-                        context.moveTo(x, y_axis.center - 5 * this.state.figure.plot_density.height)
-                        context.lineTo(x, y_axis.center + 5 * this.state.figure.plot_density.height)
+                        x_axis_path.moveTo(x, y_axis.center - 5 * this.state.figure.plot_density.height)
+                        x_axis_path.lineTo(x, y_axis.center + 5 * this.state.figure.plot_density.height)
                     }
                 }
+                x_axis.path = x_axis_path
+                context.stroke(x_axis_path)
             }
             // Drawing vertical
             if (y_axis.visible) {
+                let y_axis_path = new Path2D()
                 // Style
                 context.lineWidth = y_axis.width * this.state.figure.plot_density.width
                 context.strokeStyle = y_axis.color
                 // Axis line
-                context.moveTo(x_axis.center, 0)
-                context.lineTo(x_axis.center, this.state.figure.get_height())
+                y_axis_path.moveTo(x_axis.center, 0)
+                y_axis_path.lineTo(x_axis.center, this.state.figure.get_height())
                 // Vertical labels (height levels)
                 if (y_axis.labels.visible) {
                     context.font = y_axis.get_labels_font(this.state.figure.plot_density.width)
@@ -291,37 +311,43 @@ export default class Plot extends React.Component {
                             )
                         }
                         // Horizontal hatch
-                        context.moveTo(x_axis.center - 5 * this.state.figure.plot_density.width, y)
-                        context.lineTo(x_axis.center + 5 * this.state.figure.plot_density.width, y)
+                        y_axis_path.moveTo(x_axis.center - 5 * this.state.figure.plot_density.width, y)
+                        y_axis_path.lineTo(x_axis.center + 5 * this.state.figure.plot_density.width, y)
                     }
                 }
+                y_axis.path = y_axis_path
+                context.stroke(y_axis_path)
             }
-            context.stroke()
-            context.closePath()
+            this.setState({axes: {x: x_axis, y: y_axis}}, callback)
         }
     }
     // Show translucent grid
-    show_grid() {
+    show_grid(callback) {
         const context = this.canvas.current.getContext('2d')
-        const grid = this.state.grid
-        context.beginPath()
-        context.lineWidth = grid.width * this.state.figure.plot_density.height
-        context.strokeStyle = grid.color
+        let grid = this.state.grid
         // Drawing horizontal
-        for (let i = 1; i <= grid.horizontalAmount; ++i) {
-            const y = this.state.figure.get_height() * i / grid.horizontalAmount
-            context.moveTo(0, y)
-            context.lineTo(this.state.figure.get_width(), y)
+        let horizontal_grid_path = new Path2D()
+        context.lineWidth = grid.horizontal.width * this.state.figure.plot_density.height
+        context.strokeStyle = grid.horizontal.color
+        for (let i = 1; i <= grid.horizontal.amount; ++i) {
+            const y = this.state.figure.get_height() * i / grid.horizontal.amount
+            horizontal_grid_path.moveTo(0, y)
+            horizontal_grid_path.lineTo(this.state.figure.get_width(), y)
         }
-        context.lineWidth = grid.width * this.state.figure.plot_density.width
+        grid.horizontal.path = horizontal_grid_path
+        context.stroke(horizontal_grid_path)
         // Drawing vertical
-        for (let i = 1; i <= grid.verticalAmount; ++i) {
-            const x = this.state.figure.get_width() * i / grid.verticalAmount
-            context.moveTo(x, 0)
-            context.lineTo(x, this.state.figure.get_height())
+        let vertical_grid_path = new Path2D()
+        context.lineWidth = grid.vertical.width * this.state.figure.plot_density.width
+        context.strokeStyle = grid.vertical.color
+        for (let i = 1; i <= grid.vertical.amount; ++i) {
+            const x = this.state.figure.get_width() * i / grid.vertical.amount
+            vertical_grid_path.moveTo(x, 0)
+            vertical_grid_path.lineTo(x, this.state.figure.get_height())
         }
-        context.stroke()
-        context.closePath()
+        grid.vertical.path = vertical_grid_path
+        context.stroke(vertical_grid_path)
+        this.setState({grid: grid}, callback)
     }
     // Changing coordinates system center X0 = (x0, y0)
     set_coordinates_system(callback) {
