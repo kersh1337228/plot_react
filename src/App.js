82876ba95e51644c24eed9c76440d54dad5dddc3
kersh1337228 <antonycherevko@gmail.com>
2022-04-30 18:52:44 +0300

    Code optimization.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import Plot from './Plot'
 
 
-function linspace(a, b, number=(b - a) / 0.01) {
-  let range = []
-  for (let i = a; i <= b; i += (b - a) / number) {
-    range.push(i)
-  }
-  return range
-}
-
-
 function App() {
-    // let x = linspace(-10, 10)
-    // let y = []
-    // x.forEach(xs => y.push(Math.sin(xs)))
     // let data = [
     //   {date: '10-10-2002', balance: 0},
     //   {date: '11-10-2002', balance: 200},
--- src/Plot.jsx
+++ src/Plot.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
 
-// Python-like zip function
-function zip(...args) {
-    return [...args[0]].map((_, i) => args.map(arg => arg[i]))
-}
-
-
 // Figure inner axis class
 class Axis {
     constructor() {
@@ -100,130 +94,75 @@ export default class Plot extends React.Component {
             context: null,
         }
         this.type = props.type
-        switch(this.type) {
-            case 'date_value':
-                this.state.balance = Array.from(this.state.data, obj => obj.balance)
-                break
-            case 'financial':
-                [this.state.low, this.state.high] = [
-                    Array.from(Object.values(this.state.data), obj => obj.low),
-                    Array.from(Object.values(this.state.data), obj => obj.high),
-                ]
-                break
-            default:
-                throw Error('Wrong plot type specifier')
-        }
         this.canvas = React.createRef()
-    }
-    // Find the difference between window sizes and canvas sizes
-    set_scale(callback) {
-        let figure = this.state.figure
-        switch(this.type) {
-            case 'date_value':
-                figure.scale.height = (figure.get_height() * (1 - figure.padding.bottom - figure.padding.top)) /
-                    Math.abs(Math.max.apply(null, this.state.balance) - Math.min.apply(null, this.state.balance))
-                figure.scale.width = (figure.get_width() * (1 - figure.padding.left - figure.padding.right)) / (this.state.balance.length - 1)
-                break
-            case 'financial':
-                figure.scale.height = (figure.get_height() * (1 - figure.padding.bottom - figure.padding.top)) /
-                    Math.abs(Math.max.apply(null, this.state.high) - Math.min.apply(null, this.state.low))
-                figure.scale.width = (figure.get_width() * (1 - figure.padding.left - figure.padding.right)) / (this.state.low.length)
-                break
-        }
-        // Applying changes
-        this.setState({figure: figure}, callback)
-    }
-    // Rescales window and canvas itself
-    set_window() {
-        let canvas = this.canvas.current
-        // Window
-        canvas.style.width = `${this.state.figure.window_size.width}px`
-        canvas.style.height = `${this.state.figure.window_size.height}px`
-        // Canvas
-        canvas.width = this.state.figure.get_width()
-        canvas.height = this.state.figure.get_height()
-    }
-    // Changing coordinates system center X0 = (x0, y0)
-    set_coordinates_system(callback) {
-        let axes = this.state.axes
-        switch (this.type) { // Window left bottom corner
-            case 'date_value':
-                axes.y.center = Math.max.apply(null, this.state.balance) *
-                    this.state.figure.scale.height + this.state.figure.padding.top * this.state.figure.get_height()
-                break
-            case 'financial':
-                axes.y.center = Math.max.apply(null, this.state.high) *
-                    this.state.figure.scale.height + this.state.figure.padding.top * this.state.figure.get_height()
-                break
-        }
-        axes.x.center = this.state.figure.padding.left * this.state.figure.window_size.width
-        // Applying changes
-        this.setState({axes: axes}, callback)
-    }
-    // Drawing plot depending on plot type
-    plot() {
-        // Rescaling based on original input data
-        this.setState({context: this.canvas.current.getContext('2d')}, () => {
-            this.set_scale(() => {
-                this.show_grid(() => {
-                    this.set_coordinates_system({
-                        'date_value': this.plot_date_value,
-                        'financial': this.plot_financial,
-                    }[this.type]) // Choosing plot method depending on plot type
-                })
-            })
-        })
     }
     // Date-value type plot (<date:String>, <value:Number>)
     plot_date_value() {
-        const context = this.state.context
-        context.save()
-        context.translate(this.state.axes.x.center, this.state.axes.y.center)
-        context.scale(this.state.figure.scale.width, -this.state.figure.scale.height)
+        let state = this.state
+        const balance = Array.from(this.state.data, obj => obj.balance)
+        // Rescaling
+        state.figure.scale.height = (state.figure.get_height() * (1 - state.figure.padding.bottom - state.figure.padding.top)) /
+            Math.abs(Math.max.apply(null, balance) - Math.min.apply(null, balance))
+        state.figure.scale.width = (state.figure.get_width() * (1 - state.figure.padding.left - state.figure.padding.right)) / (balance.length - 1)
+        // Moving coordinates system
+        state.axes.y.center = Math.max.apply(null, balance) *
+            state.figure.scale.height + state.figure.padding.top * state.figure.get_height()
+        state.axes.x.center = state.figure.padding.left * state.figure.window_size.width
+        state.context.save()
+        state.context.translate(state.axes.x.center, state.axes.y.center)
+        state.context.scale(state.figure.scale.width, -state.figure.scale.height)
         // Drawing plot
         let figure_path = new Path2D()
-        context.lineWidth = this.state.figure.get_line_width()
-        context.strokeStyle = this.state.figure.style.color
-        context.lineJoin = 'round'
-        figure_path.moveTo(0, this.state.balance[0])
-        for (let i = 1; i < this.state.balance.length; ++i) {
-            figure_path.lineTo(i, this.state.balance[i])
+        state.context.lineWidth = state.figure.get_line_width()
+        state.context.strokeStyle = state.figure.style.color
+        state.context.lineJoin = 'round'
+        figure_path.moveTo(0, state.balance[0])
+        for (let i = 1; i < state.balance.length; ++i) {
+            figure_path.lineTo(i, state.balance[i])
         }
-        context.stroke(figure_path)
+        state.context.stroke(figure_path)
         // Saving figure path
-        let figure = this.state.figure
-        figure.path = figure_path
-        context.restore() // Restoring context
-        this.setState({figure: figure, context:context})
+        state.figure.path = figure_path
+        state.context.restore() // Restoring context
+        this.setState(state)
     }
     // Financial type plot (
     //      <date:String>,
     //      (<open:Number>, <high:Number>, <low:Number>, <close:Number>, <volume:Number>)
     // )
     plot_financial() {
-        const context = this.state.context
-        context.save()
-        context.translate(this.state.axes.x.center, this.state.axes.y.center)
-        context.scale(1, -this.state.figure.scale.height)
-        context.lineJoin = 'round'
-        let i = 0
-        for (const [date, {open, high, low, close, volume}] of Object.entries(this.state.data)) {
-            this.draw_candle(i, open, high, low, close)
-            ++i
+        let state = this.state
+        const [lows, highs] = [
+            Array.from(Object.values(this.state.data), obj => obj.low),
+            Array.from(Object.values(this.state.data), obj => obj.high),
+        ]
+        // Rescaling
+        state.figure.scale.height = (state.figure.get_height() * (1 - state.figure.padding.bottom - state.figure.padding.top)) /
+            Math.abs(Math.max.apply(null, highs) - Math.min.apply(null, lows))
+        state.figure.scale.width = (state.figure.get_width() * (1 - state.figure.padding.left - state.figure.padding.right)) / (highs.length)
+        // Moving coordinates system
+        state.axes.y.center = Math.max.apply(null, highs) *
+            state.figure.scale.height + state.figure.padding.top * state.figure.get_height()
+        state.axes.x.center = state.figure.padding.left * state.figure.window_size.width
+        state.context.save()
+        state.context.translate(state.axes.x.center, state.axes.y.center)
+        state.context.scale(1, -state.figure.scale.height)
+        // Drawing plot
+        state.context.lineJoin = 'round'
+        const data = Object.values(state.data)
+        for (let i = 0; i < data.length; ++i) {
+            const {open, high, low, close, volume} = data[i]
+            const style = close - open > 0 ? '#53e9b5' : '#da2c4d'
+            state.context.beginPath()
+            state.context.strokeStyle = style
+            state.context.moveTo((2 * i + 1) * state.figure.scale.width / 2, low)
+            state.context.lineTo((2 * i + 1) * state.figure.scale.width / 2, high)
+            state.context.stroke()
+            state.context.fillStyle = style
+            state.context.fillRect(i * this.state.figure.scale.width , open, this.state.figure.scale.width, close - open)
+            state.context.closePath()
         }
-        context.restore()
-    }
-    draw_candle(date, open, high, low, close) {
-        const context = this.state.context
-        const style = close - open > 0 ? '#53e9b5' : '#da2c4d'
-        context.beginPath()
-        context.strokeStyle = style
-        context.moveTo((2 * date + 1) * this.state.figure.scale.width / 2, low )
-        context.lineTo((2 * date + 1) * this.state.figure.scale.width / 2, high)
-        context.stroke()
-        context.fillStyle = style
-        context.fillRect(date * this.state.figure.scale.width , open, this.state.figure.scale.width, close - open)
-        context.closePath()
+        state.context.restore()
     }
     // Show translucent grid
     show_grid(callback) {
@@ -255,8 +194,17 @@ export default class Plot extends React.Component {
     }
 
     componentDidMount() {
-        this.set_window()
-        this.plot()
+        let canvas = this.canvas.current
+        canvas.style.width = `${this.state.figure.window_size.width}px`
+        canvas.style.height = `${this.state.figure.window_size.height}px`
+        canvas.width = this.state.figure.get_width()
+        canvas.height = this.state.figure.get_height()
+        this.setState({context: this.canvas.current.getContext('2d')}, () => {
+            this.show_grid({
+                'date_value': this.plot_date_value,
+                'financial': this.plot_financial,
+            }[this.type]) // Choosing plot method depending on plot type
+        })
     }
 
     componentDidUpdate() {
